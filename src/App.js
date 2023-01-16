40149408592918f273ef570f33c3f4aab642065b
naekit <mykyta.onishchuck@code.berlin>
2023-01-16 10:44:03 -0500

    db update w useEffect and notification component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { useSelector } from "react-redux"
 import Cart from "./components/Cart/Cart"
 import Layout from "./components/Layout/Layout"
@@ -5,6 +6,13 @@ import Products from "./components/Shop/Products"
 
 function App() {
 	const cartVisible = useSelector((state) => state.ui.cartVisible)
+	const cart = useSelector((state) => state.cart)
+	useEffect(() => {
+		fetch("https://react-ref-async-default-rtdb.firebaseio.com/cart.json", {
+			method: "PUT",
+			body: JSON.stringify(cart),
+		})
+	}, [cart])
 
 	return (
 		<Layout>
